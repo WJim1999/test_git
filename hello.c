baeac3111d65d778ee9ef07aea43be5a9985eee0
王健 <wangjian19990806@gmail.com>
2024-08-02 09:20:23 +0800

    Add hello.h and modify hello.c for main branch

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ using namespace std;
 int main () {
 	int n;
 	cin >> n;
-	cout << "My test hello world:" << n << endl; 
+	cout << "My test hello world:" << n << endl;
+	return 0;
 }
---
+++ hello.h
@@ -0,0 +1,9 @@
+#ifndef _HELLO_H_
+#define
+
+#define DDR (0x0000FFFFU)
+
+
+
+
+#endif
